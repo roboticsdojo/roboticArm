90097e7e38a572ff42be54f8d8e4300fd536f0f1
codewithlennylen <codewithlennylen254@gmail.com>
2023-06-05 12:33:48 +0300

    YOLOv8 Dataset + Model

---
+++ resources/vision/object_detection/using-yolov8/model-training/ROBOKEN-Object-Detection.v1i.yolov8/infer.py
@@ -0,0 +1,14 @@
+# pip install roboflow
+from roboflow import Roboflow
+rf = Roboflow(api_key="API_KEY")
+project = rf.workspace().project("MODEL_ENDPOINT")
+model = project.version(VERSION).model
+
+# infer on a local image
+print(model.predict("your_image.jpg", confidence=40, overlap=30).json())
+
+# visualize your prediction
+# model.predict("your_image.jpg", confidence=40, overlap=30).save("prediction.jpg")
+
+# infer on an image hosted elsewhere
+# print(model.predict("URL_OF_YOUR_IMAGE", hosted=True, confidence=40, overlap=30).json())
