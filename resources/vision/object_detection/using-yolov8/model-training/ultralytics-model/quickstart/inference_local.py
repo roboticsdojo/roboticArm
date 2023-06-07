2eb1fcd4d92fa6cc57bb2e2a6dc14a5e31468ce7
codewithlennylen <codewithlennylen254@gmail.com>
2023-06-07 23:27:01 +0300

    YOLOv8 Ultralytics Model

--- resources/vision/object_detection/using-yolov8/model-training/ultralytics-model/quickstart/inference_local.py
+++ resources/vision/object_detection/using-yolov8/model-training/ultralytics-model/quickstart/inference_local.py
@@ -9,6 +9,9 @@ model = YOLO('yolov8n.pt')
 # Train the model using the 'coco128.yaml' dataset for 3 epochs
 # results = model.train(data='coco128.yaml', epochs=3)
 
+# Train YOLOv8n on the COCO128 dataset for 100 epochs at image size 640
+# model.train(data='coco128.yaml', epochs=100, imgsz=640)
+
 # Evaluate the model's performance on the validation set
 # results = model.val()
 
