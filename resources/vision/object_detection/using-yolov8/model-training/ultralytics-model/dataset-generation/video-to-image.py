b7d435b8c1d68e9d567f14f52fe62e5a3d5ad7c8
codewithlennylen <codewithlennylen254@gmail.com>
2023-06-08 07:54:56 +0300

    Video-to-Image Capture Ready

---
+++ resources/vision/object_detection/using-yolov8/model-training/ultralytics-model/dataset-generation/video-to-image.py
@@ -0,0 +1,46 @@
+import cv2
+import time
+import random
+
+video = cv2.VideoCapture(0)
+
+count = 0
+frame_count = 0
+random.seed(time.time())
+
+print("Press SPACE Key to start Video-to-Image Capture")
+
+while True:
+    ret, frame = video.read()
+
+    if not ret:
+        print("Failed to get frame")
+        break
+
+    fps = video.get(cv2.CAP_PROP_FPS)
+    cv2.imshow(f"Frame: {fps} fps", frame)
+
+    struct_time_tuple = time.localtime()  # get struct_time
+    time_string = time.strftime("%H:%M:%S", struct_time_tuple)
+
+    count += 1
+
+    key = cv2.waitKey(1)
+    # SPACE pressed
+    if key % 256 == 32:
+        # At 30 fps, get 10 frames every second by only storing frames divisible by 3
+        if count % 3 == 0:
+            cv2.imwrite(
+                f'./images/image_{frame_count}_{random.randrange(200)}.jpg', frame)
+            print(f"t :> {time_string} | {count} | {frame_count}")
+            frame_count += 1
+
+    if frame_count == 100:
+        print(f"Video-to-Image Capture complete | Frames: {frame_count}")
+        break
+
+    if key == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
