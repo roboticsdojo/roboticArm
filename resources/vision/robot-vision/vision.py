b33bf3d78aed863ef3e9aef92e3fbebb70120571
codewithlennylen <codewithlennylen254@gmail.com>
2023-06-13 14:27:35 +0300

    Video Stream

--- resources/vision/robot-vision/vision.py
+++ resources/vision/robot-vision/vision.py
@@ -3,7 +3,6 @@ import cv2
 import numpy as np
 
 
-
 # Initialize Inference Model
 # rf = Roboflow(api_key="8kLxlVgusB4R1fsKc2DX")
 # project = rf.workspace().project("roboken-object-detection")
@@ -16,19 +15,39 @@ cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 640)
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    if ret:
-        cv2.imwrite('camera_snapshot.jpg', frame)
+
+def take_snapshot():
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret:
+            cv2.imwrite('camera_snapshot.jpg', frame)
+            cv2.imshow("Raspberry Pi Camera V2", frame)
+            cv2.waitKey(0)
+
+        print("Failed to Capture")
+        break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+def video_stream():
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            print("Failed to Capture")
+            break
+
         cv2.imshow("Raspberry Pi Camera V2", frame)
-        cv2.waitKey(0)
-        
-    print("Failed to Capture")
-    break
 
-cap.release()
-cv2.destroyAllWindows()
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+video_stream()
 
 # # test-image
 # test_image = 'test_image.jpg'
