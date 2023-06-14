21ae535928d59c1b74041fd8f491e4b6ed5528b8
codewithlennylen <codewithlennylen254@gmail.com>
2023-06-14 11:04:31 +0300

    Infer from Camera

--- resources/vision/robot-vision/vision.py
+++ resources/vision/robot-vision/vision.py
@@ -48,6 +48,27 @@ def video_stream():
     cv2.destroyAllWindows()
 
 
+def infer_from_snapshot():
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret:
+            # cv2.imwrite('infer_from_snapshot.jpg', frame)
+            # cv2.imshow("Raspberry Pi Camera V2", frame)
+            # cv2.waitKey(0)
+
+            # Infer on snapshot
+            result = model.predict(frame, confidence=40, overlap=30)
+
+            # Print result
+            print(result.json())
+
+        print("Failed to Capture")
+        break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
 def infer_from_video_stream():
     while cap.isOpened():
         ret, frame = cap.read()
@@ -56,15 +77,16 @@ def infer_from_video_stream():
             break
 
         # Show video stream
-        cv2.imshow("Raspberry Pi Camera V2", frame)
+        # cv2.imshow("Raspberry Pi Camera V2", frame)
 
         # Infer on video stream (frame by frame)
         result = model.predict(frame, confidence=40, overlap=30)
 
-        # print(result.json())
-        print(f"Prediction_Result: {result.json()['predictions']}")
+        print(result.json())
+        # print(f"Prediction_Result: {result.json()['predictions']}")
 
         #! Save result as image (visualize) [FAILED]
+        # ? Convert to image object
         # if result.json()['predictions']:
         #     result.save("video_inference_result.jpg")
         #     print("Save result as image [SUCCESS]")
@@ -80,7 +102,9 @@ def infer_from_video_stream():
     cv2.destroyAllWindows()
 
 
-infer_from_video_stream()
+# infer_from_video_stream()
+infer_from_snapshot()
+# video_stream()
 
 # # test-image
 # test_image = 'test_image.jpg'
